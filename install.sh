49a68c6193fec3172f79168b406bd11353f11e95
Ankit Patel <dr.bathman@gmail.com>
2019-09-09 18:59:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -123,7 +123,7 @@ REPLACE="
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Zenfone Bootanimation    "
+  ui_print "     Asus Bootanimation    "
   ui_print "*******************************"
 }
 
